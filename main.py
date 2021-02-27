ae3772c45d912160927a6e8a28c9c43df8cd3037
bulk <grur.ura@mail.ru>
2021-02-27 17:12:43 +0500

    added extension from base

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('base.html', title='Заготовка')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
